8b01ae1e200970e623578a0264e7b660a2da3b8f
Eba Fekadu <ebafekadu@gmail.com>
2024-04-01 14:39:35 +0300

    Delete and update functionality added

--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { createPost, getposts} from '../controllers/post.controller.js'
+import { createPost, getposts, deletepost, updatepost} from '../controllers/post.controller.js'
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/create-post', verifyToken, createPost)
 router.get('/getposts', getposts)
+router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
+router.put('/updatepost/:postId/:userId', verifyToken, updatepost)
 
 export default router
